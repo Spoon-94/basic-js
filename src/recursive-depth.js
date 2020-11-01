0accce63d62848efa5075035b5e7e1b25bd74f31
Dima Paymushkin <Dimon-94@yandex.ru>
2020-11-02 00:05:35 +0400

    feat: complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
+let maxDepth = 0;
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+   calculateDepth(arr, currentDepth = 1) {
+      let depth = 0;
+      for (let i = 0; i < arr.length; i++) {
+         if(Array.isArray(arr[i])) {
+            maxDepth = this.calculateDepth(arr[i], currentDepth + 1);
+         }
+      }
+      if (maxDepth > currentDepth) {
+         depth = maxDepth; 
+      } else {
+         depth = currentDepth;
+      }
+      maxDepth = 0;
+      return depth;
+   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
+const ALPHABET = 26;
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direct = true) {
+        this.isDirect = direct;
+    }
+    encrypt(message, key) {
+        if (arguments.length <= 1) {
+            throw new Error("Incorrect arguments");
+        } 
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let arr = [];
+        let len = message.length;
+        let shift = 0;
+        for(let i = 0; i < len; i++) {
+            let code = message[i].charCodeAt(0);
+            if(code >= 65 && code <= 90) {
+                let newSim = String.fromCharCode((code + key.charCodeAt(shift)) % ALPHABET + "A".charCodeAt(0));
+                arr.push(newSim);
+                if(shift != key.length - 1) shift++;
+                else shift = 0;
+            }
+            else arr.push(message[i]);         
+        }
+        if(this.isDirect == false) {
+            arr.reverse();
+        }
+        return arr.join("");        
+    }
+    decrypt(encryptedMessage, key) {
+        if(arguments.length <= 1) {
+            throw new Error("Incorrect arguments");
+        }
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+        let arr = [];
+        let len = encryptedMessage.length;
+        let shift = 0;
+        for(let i = 0; i < len; i++) {
+            let code = encryptedMessage[i].charCodeAt(0);
+            if(code >= 65 && code <= 90) {
+                let newSim = String.fromCharCode((code + ALPHABET - key.charCodeAt(shift)) % ALPHABET + "A".charCodeAt(0));
+                arr.push(newSim);
+                if(shift != key.length - 1) {
+                   shift++; 
+                } else {
+                    shift = 0;
+                }
+            }
+            else arr.push(encryptedMessage[i]);         
+        }
+        if(this.isDirect == false) {
+            arr.reverse();
+        }
+        return arr.join("");
+    }
 }
+
 
 module.exports = VigenereCipheringMachine;
